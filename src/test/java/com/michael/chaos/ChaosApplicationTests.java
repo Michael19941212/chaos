e8a40e4d7aee68240adba73623c7569469027ff7
zhongjinpeng <jpzhong@gmail.com>
2024-03-12 17:38:35 +0800

    init

---
+++ src/test/java/com/michael/chaos/ChaosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.chaos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
